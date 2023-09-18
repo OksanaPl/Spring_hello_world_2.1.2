7701c70971f4625f1ae998bbde76360a0344d98e
OksanaPl <ok965@yandex.ru>
2023-09-18 20:58:58 +0300

    Java pre-project. Практическая задача 2.1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,25 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        System.out.println(bean == bean1);
+
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        bean3.setName("Barsik");
+        System.out.println(bean3.getName());
+        Cat bean4 =
+                (Cat) applicationContext.getBean("cat");
+        bean4.setName("Ponchik");
+        System.out.println(bean4.getName());
+        System.out.println(bean3==bean4);
+
     }
 }
